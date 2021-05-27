98f96351e17195ded749e67522aa2433645b5dbb
lavashikaa <lavashikaa@gmail.com>
2021-05-27 12:05:14 +0300

    f

---
+++ script/script.js
@@ -0,0 +1,19 @@
+let name = "Джон";
+let admin = name;
+alert(admin);
+
+
+
+
+
+
+let ourPlanet = "Earth";
+let currentUser = "user";
+
+
+
+
+let x = "10";
+let y = 10;
+console.log(x + y, typeof (x + y));
+// потому что если прибовлять строчные значение к не строчным то они все станут строчными
